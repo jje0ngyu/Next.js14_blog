d2fdc642bf98f746b02fd31cc3682003833c1e1b
jje0ngyu <naniaerang@naver.com>
2023-12-27 20:20:06 +0900

    Carousel 기능 추가

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import CarouselPosts from "@/components/CarouselPosts";
 import FeaturedPosts from "@/components/FeaturedPosts";
 import Hero from "@/components/Hero";
 
@@ -7,6 +8,7 @@ export default function Home() {
   <>
     <Hero />
     <FeaturedPosts />
+    <CarouselPosts />
   </>
   );
 }
--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -5,7 +5,7 @@ export default function PostCard({
   post: { title, description, date, category, path },
 }) {
   return (
-    <article className="rounded-md overflow-hidden shadow-lg">
+    <article className="rounded-md overflow-hidden shadow-md hover:shadow-xl">
       <Link href={`/post/${path}`}>
         <Image
           className="w-full"
@@ -15,7 +15,7 @@ export default function PostCard({
           height={200}
         />
         <div className="flex flex-col items-center p-4">
-          <time className="self-end">{date.toString()}</time>
+          <time className="self-end text-gray-700">{date.toString()}</time>
           <h3 className="text-lg font-bold">{title}</h3>
           <p className="w-full truncate text-center">{description}</p>
           <span className="text-sm rounded-lg bg-green-100 px-2 py-1 mt-2">{category}</span>
